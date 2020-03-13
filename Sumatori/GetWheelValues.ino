4484b2f7f82785cac1cf3256c7a021de104034b4
Carter Cocke <carter.cocke@utah.edu>
2020-03-12 20:48:04 -0600

    Initial commit (untested)

---
+++ Sumatori/GetWheelValues.ino
@@ -0,0 +1,45 @@
+void GetWheelValues(){
+
+  // LEFT WHEEL CALCULATIONS
+  startTimeLeft = millis();
+  
+  // Get pos and vel of gearbox
+  leftWheelPosition = 2 * M_PI * leftEncoderCount / (gearRatio * countsPerRev);
+  leftWheelVelocity = 1000 * (leftWheelPosition - leftWheelPositionOld) / (startTimeLeft - endTimeLeft);
+
+  #ifdef sensor // Print positions and velocities for debugging
+    delay(50);
+    Serial.print("Left: P = ");
+    Serial.print(leftWheelPosition);
+    Serial.print(" V = ");
+    Serial.print(leftWheelVelocity);
+    Serial.println(); 
+  #endif
+
+  // Store data for derivs
+  leftWheelPositionOld = leftWheelPosition;
+  endTimeLeft = startTimeLeft;
+
+
+  // RIGHT WHEEL CALCULATIONS
+  startTimeRight = millis();
+  
+  // Get pos and vel of gearbox
+  rightWheelPosition = 2 * M_PI * rightEncoderCount / (gearRatio * countsPerRev);
+  rightWheelVelocity = 1000 * (rightWheelPosition - rightWheelPositionOld) / (startTimeRight - endTimeRight);
+
+  #ifdef sensor // Print positions and velocities for debugging
+    delay(50);
+    Serial.print("Right: P = ");
+    Serial.print(rightWheelPosition);
+    Serial.print(" V = ");
+    Serial.print(rightWheelVelocity);
+    Serial.println(); 
+  #endif
+
+  // Store data for derivs
+  rightWheelPositionOld = rightWheelPosition;
+  endTimeRight = startTimeRight;
+  
+}
+
